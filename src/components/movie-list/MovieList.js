531c5c750926b23ce559c08972439f179a56ad86
Maya, Sebastiao <sebastiao.maya@sap.com>
2019-07-27 18:26:59 +0100

    Restructured code

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -3,31 +3,20 @@ import Movie from '../movie/Movie';
 
 export default class MovieList extends Component {
   render() {
-    const moviesKeys = Object.keys(this.props.movies);
+    const movies = Object.keys(this.props.movies).map(key => {
+      let movie = this.props.movies[key];
+      return (
+        <Movie
+          key={movie.id}
+          name={movie.name}
+          favoritedBy={this.isMovieFavorite(key, this.props.profiles)}
+          users={this.props.users}
+        />
+      );
+    });
 
-    return (
-      <div>
-        {moviesKeys.map(key => {
-          let movie = this.props.movies[key];
-          return (
-            <Movie
-              key={movie.id}
-              name={movie.name}
-              favoritedBy={this.isMovieFavorite(key, this.props.profiles)}
-              users={this.props.users}
-            />
-          );
-        })}
-      </div>
-    );
+    return <div>{movies}</div>;
   }
-
-  /* {this.props.movies.map(movie => (
-          <Movie
-            id={movie.id}
-            favorited={this.isMovieFavorite(movie.id, this.props.profiles)}
-          />
-        ))} */
 
   isMovieFavorite(movieId, profiles) {
     let profilesWithFavoriteMovie = profiles.filter(
--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -3,40 +3,32 @@ import User from '../user/User';
 
 export default class Movie extends Component {
   render() {
+    /* IIFE */
+    const favoritedByUsers = (favoritedBy => {
+      if (favoritedBy.length !== 0) {
+        return (
+          <ul>
+            {favoritedBy.map(profile => {
+              return (
+                <User
+                  key={`${profile.userID}${this.props.name}`}
+                  userObject={this.props.users[profile.userID]}
+                />
+              );
+            })}
+          </ul>
+        );
+      } else {
+        return <p>None of the current users liked this movie</p>;
+      }
+    })(this.props.favoritedBy);
+
     return (
       <div>
         <h2>{this.props.name}</h2>
         <p>Liked By:</p>
-        {(favoritedBy => {
-          if (favoritedBy.length !== 0) {
-            return (
-              <ul>
-                {favoritedBy.map(profile => {
-                  return (
-                    <User
-                      key={`${profile.userID}${this.props.name}`}
-                      userObject={this.props.users[profile.userID]}
-                    />
-                  );
-                })}
-              </ul>
-            );
-          } else {
-            return <p>None of the current users liked this movie</p>;
-          }
-        })(this.props.favoritedBy)}
+        {favoritedByUsers}
       </div>
     );
   }
-
-  /* <h2>Get Out</h2>
-<p>Liked By:</p>
-<ul>
-  <li>John Doe</li>
-  <li>Autumn Green</li>
-</ul> 
-
-<h2>Autumn Green</h2>
-<p>None of the current users liked this movie</p>
-*/
 }
